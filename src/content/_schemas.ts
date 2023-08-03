dbc68cd471f161e0142ddd8e7a6768d632adf96c
Daniel Luna <lunad@bxscience.edu>
2023-08-03 08:12:50 -0400

    Add Results to Sessions

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -2,31 +2,95 @@ import z from "zod";
 
 export const sessionInfo = z.object({
   time: z.coerce.date(),
-  timeOfDay: z.union([z.literal('day'), z.literal('night')]).default('day'),
-  weather: z.union([z.literal('clear'), z.literal('partlyclear'), z.literal('cloudy'), z.literal('rain'), z.literal('heavyrain')]).optional()
-})
+  timeOfDay: z.union([z.literal("day"), z.literal("night")]).default("day"),
+  weather: z
+    .union([
+      z.literal("clear"),
+      z.literal("partlyclear"),
+      z.literal("cloudy"),
+      z.literal("rain"),
+      z.literal("heavyrain"),
+    ])
+    .optional(),
+});
+
+export const sessionResults = z.object({
+  pos: z.number(),
+  driverNum: z.number(),
+});
+
+export const qualifyingGap = z.discriminatedUnion("status", [
+  z.object({ status: z.literal("DNF") }),
+  z.object({ status: z.literal("DNS") }),
+  z.object({
+    status: z.literal("Completed"),
+    timeInSec: z.number().positive().finite(),
+  }),
+]);
+
+export const raceGap = z.discriminatedUnion("status", [
+  z.object({ status: z.literal("DNF") }),
+  z.object({ status: z.literal("DNS") }),
+  z.object({ status: z.literal("Leader") }),
+  z.object({
+    status: z.literal("Lapped"),
+    behind: z.number().int().positive().finite(),
+  }),
+  z.object({
+    status: z.literal("SameLapAsLeader"),
+    behind: z.number().positive().finite(),
+  }),
+]);
+
+export const qualSessionResults = z
+  .array(
+    sessionResults.merge(
+      z.object({
+        gap: qualifyingGap,
+      })
+    )
+  )
+  .min(3);
+
+export const raceSessionResults = z
+  .array(sessionResults.merge(z.object({ gap: raceGap })))
+  .min(3);
+
+export const competitiveSessionInfo = sessionInfo.merge(
+  z.object({
+    session: z
+      .discriminatedUnion("type", [
+        z.object({
+          type: z.literal("Qualifying"),
+          results: qualSessionResults,
+        }),
+        z.object({ type: z.literal("Race"), results: raceSessionResults }),
+      ])
+      .optional(),
+  })
+);
 
 const normalGP = z.object({
   fp1: sessionInfo,
   fp2: sessionInfo,
   fp3: sessionInfo,
-  qualifying: sessionInfo,
-  race: sessionInfo,
+  qualifying: competitiveSessionInfo,
+  race: competitiveSessionInfo,
 });
 
 const sprint2022GP = z.object({
   practice: sessionInfo,
   qualifying: sessionInfo,
-  sprint: sessionInfo,
-  race: sessionInfo,
+  sprint: competitiveSessionInfo,
+  race: competitiveSessionInfo,
 });
 
 const sprintGP = z.object({
   practice: sessionInfo,
-  qualifying: sessionInfo,
-  sprintShootout: sessionInfo,
-  sprint: sessionInfo,
-  race: sessionInfo,
+  qualifying: competitiveSessionInfo,
+  sprintShootout: competitiveSessionInfo,
+  sprint: competitiveSessionInfo,
+  race: competitiveSessionInfo,
 });
 
 const driver = z.object({
@@ -50,26 +114,22 @@ export const gpSchema = z
     weekendInfo: z
       .discriminatedUnion("weekendType", [
         z.object({ weekendType: z.literal("Normal"), sessionInfo: normalGP }),
-        z.object({ weekendType: z.literal("Sprint 2022"), sessionInfo: sprint2022GP }),
+        z.object({
+          weekendType: z.literal("Sprint 2022"),
+          sessionInfo: sprint2022GP,
+        }),
         z.object({ weekendType: z.literal("Sprint"), sessionInfo: sprintGP }),
       ])
       .optional(),
-    standings: z
-      .array(z.object({ pos: z.number(), driverNum: z.number(), gap: z.discriminatedUnion("status", [
-        z.object({ status: z.literal("DNF") }),
-        z.object({ status: z.literal("DNS") }),
-        z.object({ status: z.literal("Leader") }),
-        z.object({ status: z.literal("Lapped"), behind: z.number().int().positive().finite() }),
-        z.object({ status: z.literal("SameLapAsLeader"), behind: z.number().positive().finite() }),
-      ]) }))
-      .min(3).optional(),
-    raceInfo: z.object({
-      raceDistance: z.object({
-        numOfLaps: z.number().int().positive().finite(),
-        lapDistance: z.number().positive().finite(),
-        raceDistance: z.number().positive().finite()
+    raceInfo: z
+      .object({
+        raceDistance: z.object({
+          numOfLaps: z.number().int().positive().finite(),
+          lapDistance: z.number().positive().finite(),
+          raceDistance: z.number().positive().finite(),
+        }),
       })
-    }).optional(),
+      .optional(),
     map: z.string().optional(),
   })
   .describe("This describes a Formula 1 Weekend");
