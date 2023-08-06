d521ca6c89a0100c1db07745c0f5cff5dd13e20f
Daniel Luna <lunad@bxscience.edu>
2023-08-05 21:34:49 -0400

    Begin writing cli

--- tools/genSchedules.ts
+++ tools/genSchedules.ts
@@ -1,35 +1,23 @@
-import z from 'zod';
-import dateFormat from 'dateformat';
+import select, { Separator } from "@inquirer/select";
 
-const standingsSchema = z.object({
-
-})
-
-const weatherSchema = z.object({
-
-})
-
-const F1GPSchema = z.object({
-    name: z.string(),
-    raceType: z.union([z.literal("normal"), z.literal("sprint")]),
-    sessions: z.union([z.object({
-        fp1: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        fp2: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        fp3: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        qualifying: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        race: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy"))
-    }), z.object({
-        practice: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        qualifying: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        sprintQualifying: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        sprint: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy")),
-        race: z.string().transform((str)=>dateFormat(new Date(str), "mmmm dS, yyyy"))
-    })]),
-    standings: z.object({
-        beforeWeekend: standingsSchema,
-        afterWeekend: standingsSchema.optional()
-    }),
-    weather: weatherSchema.optional(),
+const action = await select({
+  message: "Select an Action",
+  choices: [
+    {
+      name: "Create New Race",
+      value: "createRace",
+      description: "Create a race",
+    },
+    {
+      name: "Edit Existing Race",
+      value: "editRace",
+      description: "Edit the value of a race",
+    },
+  ],
 });
 
-console.log("Test")
+async function createNewRace() {}
+
+if (action == "createRace") {
+  await createNewRace();
+}
