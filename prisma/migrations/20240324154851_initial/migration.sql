ddaaea3b223d3520834c5955fdf38e9319a61902
Daniel Luna <l2318dan@gmail.com>
2024-03-24 10:49:51 -0500

    Add initial prisma support

---
+++ prisma/migrations/20240324154851_initial/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Driver" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "Driver_pkey" PRIMARY KEY ("id")
+);
