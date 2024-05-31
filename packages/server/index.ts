73245a839aa2477e01701945abc8b5afbca1fb40
Daniel Luna <l2318dan@gmail.com>
2024-05-31 13:09:59 -0400

    Beginning to write new API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,8 @@ app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
 });
 
+app.use("/docs", express.static("docs"));
+
 app
   .listen(port, () => {
     console.log("Server running at PORT: ", port);
