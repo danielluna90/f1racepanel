1b2ccf722b6ae5d367cf1592abe8d455210a2ec0
Daniel Luna <l2318dan@gmail.com>
2024-05-31 10:21:26 -0400

    Add Express.JS

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
+});
+
+app
+  .listen(port, () => {
+    console.log("Server running at PORT: ", port);
+  })
+  .on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+  });
