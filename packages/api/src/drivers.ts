a3755e2cfb09fe0f9f8a0e11c288dd4e97ff965b
Daniel Luna <l2318dan@gmail.com>
2024-06-02 19:37:03 -0400

    Implementing the Driver API endpoints

--- packages/api/src/drivers.ts
+++ packages/api/src/drivers.ts
@@ -3,36 +3,78 @@ const DriverRoutes = express.Router();
 
 import { prisma } from "./prisma";
 
-DriverRoutes.get("/", async (req: Request, res: Response) => {
-  const drivers = await prisma.driver.findMany();
+import z from "zod";
 
-  res.send(drivers);
-});
+import { ErrorResponse, Driver } from "f1racepanel-common/src/types";
 
-DriverRoutes.get("/:DriverID", async (req: Request, res: Response) => {
-  const driverID = req.params.DriverID;
+DriverRoutes.get(
+  "/",
+  async (
+    req: Request,
+    res: Response<z.infer<typeof ErrorResponse> | z.infer<typeof Driver>[], {}>
+  ) => {
+    const drivers = await prisma.driver.findMany();
 
-  const driver = await prisma.driver.findUnique({
-    where: {
-      id: driverID,
-    },
-  });
+    res.send(drivers);
+  }
+);
 
-  res.send(driver);
-});
+DriverRoutes.get(
+  "/:DriverID",
+  async (
+    req: Request,
+    res: Response<z.infer<typeof ErrorResponse> | z.infer<typeof Driver>, {}>
+  ) => {
+    const driverID = req.params.DriverID;
 
-DriverRoutes.post("/:DriverID", async (req: Request, res: Response) => {
-  const driverID = req.params.DriverID;
+    const driver = await prisma.driver.findUnique({
+      where: {
+        id: driverID,
+      },
+    });
 
-  const driver = await prisma.driver.create({
-    data: {
-      name: "Test Driver",
-      dob: "06-02-2024",
-      nationality: "US",
-    },
-  });
+    if (driver == null) {
+      res.status(404).send({
+        code: 404,
+        description: `Could not find driver with DriverID: ${driverID}`,
+      });
 
-  res.send(driver);
-});
+      return;
+    }
+
+    res.send({
+      ...driver,
+    });
+  }
+);
+
+DriverRoutes.post(
+  "/:DriverID",
+  async (
+    req: Request,
+    res: Response<z.infer<typeof ErrorResponse> | z.infer<typeof Driver>, {}>
+  ) => {
+    const driverID = req.params.DriverID;
+
+    const driver = await prisma.driver.create({
+      data: {
+        name: "Test Driver",
+        dob: "06-02-2024",
+        nationality: "US",
+      },
+    });
+
+    if (driver == null) {
+      res.status(404).send({
+        code: 404,
+        description: `Could not find driver with DriverID: ${driverID}`,
+      });
+
+      return;
+    }
+
+    res.send(driver);
+  }
+);
 
 export default DriverRoutes;
