d38c5e1e4b93a4a0139afb8204b122f1c45fc500
Daniel Luna <l2318dan@gmail.com>
2024-06-17 13:17:38 -0400

    Add sitemap

--- packages/client-new/astro.config.mjs
+++ packages/client-new/astro.config.mjs
@@ -1,7 +1,13 @@
+import sitemap from "@astrojs/sitemap";
+
 import { defineConfig, envField } from 'astro/config';
+
+const url = "https://f1racepanel.com";
 
 // https://astro.build/config
 export default defineConfig({
+  site: url,
+  integrations: [sitemap()],
   experimental: {
     env: {
       schema: {
