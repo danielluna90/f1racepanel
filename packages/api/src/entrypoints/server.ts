3ef106c57ea55a5b94a903e144b563cece78bc57
Daniel Luna <l2318dan@gmail.com>
2024-06-20 06:50:34 -0400

    Added docker compose to server

--- packages/api/src/entrypoints/server.ts
+++ packages/api/src/entrypoints/server.ts
@@ -8,8 +8,10 @@ const isDev = process.env.IS_DEV_OR_TEST?.toLowerCase() == 'true';
 import { v2 as compose } from 'docker-compose';
 import path from 'node:path';
 
+const dockerServices = ['db', 'pgadmin'];
+
 if (isDev) {
-  await compose.upMany(['db', 'pgadmin'], {
+  await compose.upMany(dockerServices, {
     cwd: path.join(
       path.dirname(require.resolve('f1racepanel-common/package.json')),
       'docker'
@@ -18,6 +20,38 @@ if (isDev) {
   });
 }
 
-import { initializeWebServer } from '../index';
+import { connection, initializeWebServer } from '../index';
 
 initializeWebServer(port);
+
+async function closeDockerContainers() {
+  console.log('Closing Docker Containers');
+  await compose.stopMany(
+    {
+      cwd: path.join(
+        path.dirname(require.resolve('f1racepanel-common/package.json')),
+        'docker'
+      ),
+      log: true,
+    },
+    ...dockerServices
+  );
+}
+
+process.on('SIGINT', () => {
+  connection.close(() => {
+    console.log('Server Closed');
+  });
+  closeDockerContainers()
+    .finally(() => {
+      console.log('Closed Docker Containers');
+      process.exit(0);
+    })
+    .catch((err: unknown) => {
+      console.log(err);
+    });
+});
+
+process.on('exit', () => {
+  console.log('Program Exited Successfully');
+});
