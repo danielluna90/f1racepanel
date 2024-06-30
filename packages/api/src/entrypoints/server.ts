f5eeef1b85525a29051f972c282573647dbe52ff
Daniel Luna <git@danielluna.nyc>
2024-06-29 20:08:40 -0400

    Only close Docker Containers if in Dev

--- packages/api/src/entrypoints/server.ts
+++ packages/api/src/entrypoints/server.ts
@@ -26,38 +26,40 @@ if (isDev) {
 
 initializeWebServer(port);
 
-async function closeDockerContainers() {
-  console.log('Closing Docker Containers');
-  await compose.stopMany(
-    {
-      cwd: path.join(
-        path.dirname(require.resolve('f1racepanel-common/package.json')),
-        'docker'
-      ),
-      log: true,
-    },
-    ...dockerServices
-  );
-}
+if (isDev) {
+  async function closeDockerContainers() {
+    console.log('Closing Docker Containers');
+    await compose.stopMany(
+      {
+        cwd: path.join(
+          path.dirname(require.resolve('f1racepanel-common/package.json')),
+          'docker'
+        ),
+        log: true,
+      },
+      ...dockerServices
+    );
+  }
 
-const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTRAP', 'SIGKILL'];
+  const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTRAP', 'SIGKILL'];
 
-exitSignals.forEach((signal: NodeJS.Signals) => {
-  process.on(signal, () => {
-    connection.close(() => {
-      console.log('Server Closed');
-    });
-    closeDockerContainers()
-      .finally(() => {
-        console.log('Closed Docker Containers');
-        process.exit(0);
-      })
-      .catch((err: unknown) => {
-        console.log(err);
+  exitSignals.forEach((signal: NodeJS.Signals) => {
+    process.on(signal, () => {
+      connection.close(() => {
+        console.log('Server Closed');
       });
+      closeDockerContainers()
+        .finally(() => {
+          console.log('Closed Docker Containers');
+          process.exit(0);
+        })
+        .catch((err: unknown) => {
+          console.log(err);
+        });
+    });
   });
-});
 
-process.on('exit', () => {
-  console.log('Program Exited Successfully');
-});
+  process.on('exit', () => {
+    console.log('Program Exited Successfully');
+  });
+}
