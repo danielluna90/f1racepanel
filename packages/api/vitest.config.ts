14331f44e60428a74b30c7a2fdef0423370525d4
Daniel Luna <git@danielluna.nyc>
2024-07-12 21:20:10 -0400

    Add support for vitest ui

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      enabled: true,
       provider: 'istanbul',
-      exclude: ['src/entrypoints/**'],
+      exclude: ['src/entrypoints/**', 'html/**', 'tests/**'],
+      reportsDirectory: './html/coverage',
     },
     globalSetup: './tests/utils/global-setup.ts',
+    reporters: ['default', 'html'],
   },
 });
