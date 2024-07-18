e4ea2fcfc3cd269c3dcfbc8f4ae66de4b8277dc8
Daniel Luna <git@danielluna.nyc>
2024-07-18 19:06:33 -0400

    Create all DatabaseTypes and ObjectTypes

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,6 +1,5 @@
 export { PrismaClient, Prisma } from "@prisma/client";
 
-export { ErrorResponse, CircuitLayout, Circuit, GPWeekend, Season } from "./src/types";
-export { DatabaseTypes, ObjectTypes } from './src/types';
+export { DatabaseTypes, ObjectTypes, ErrorResponse } from './src/types';
 
 export { checkENVFileExists } from "./src/utils/checkENV";
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -11,6 +11,52 @@ namespace DatabaseTypes {
 
   export const Drivers = getPagedResponse<typeof Driver>(Driver);
   export type Drivers = z.infer<typeof Drivers>;
+
+  export const CircuitLayout = z.object({
+    id: z.string().uuid(),
+    name: z.string(),
+    first_year: z.number().int(),
+    last_year: z.number().int(),
+    track_length: z.number(),
+    race_lap_record: z.object({
+      time: z.string().regex(new RegExp("^d{1}:d{2}.d{3}$")),
+      driver_id: z.string().uuid(),
+    }),
+  });
+  export type CircuitLayout = z.infer<typeof CircuitLayout>;
+  
+  export const Circuit = z.object({
+    id: z.string().uuid(),
+    name: z.string(),
+    country: z.string().length(2),
+    date_opened: z.string().date(),
+    layout: CircuitLayout.array().min(1),
+  });
+  export type Circuit = z.infer<typeof Circuit>;
+  
+  export const GPWeekend = z.object({
+    id: z.string().uuid(),
+    start_date: z.string().date(),
+    end_date: z.string().date(),
+    circuit: z.string().uuid(),
+    sessions: z.string().uuid().array().min(1),
+    driver_entries: z.string().uuid().array().min(1),
+    team_entries: z.string().uuid().array().min(1),
+  });
+  export type GPWeekend = z.infer<typeof GPWeekend>;
+  
+  export const PointSystem = z.object({
+    awards_extra_point_for_fastest_lap_t10: z.boolean(),
+  });
+  export type PointSystem = z.infer<typeof PointSystem>;
+  
+  export const Season = z.object({
+    drivers: z.string().uuid().array(),
+    num_of_rounds: z.number().int(),
+    is_current_season: z.boolean().default(false),
+    point_system: PointSystem,
+  });
+  export type Season = z.infer<typeof Season>;
 };
 
 namespace ObjectTypes {
@@ -19,6 +65,21 @@ namespace ObjectTypes {
 
   export const Drivers = getPagedResponse<typeof Driver>(Driver);
   export type Drivers = z.infer<typeof Drivers>;
+
+  export const CircuitLayout = DatabaseTypes.CircuitLayout.omit({ id: true });
+  export type CircuitLayout = z.infer<typeof CircuitLayout>;
+
+  export const Circuit = DatabaseTypes.Circuit.omit({ id: true });
+  export type Circuit = z.infer<typeof Circuit>;
+
+  export const GPWeekend = DatabaseTypes.GPWeekend.omit({ id: true });
+  export type GPWeekend = z.infer<typeof GPWeekend>;
+
+  export const PointSystem = DatabaseTypes.PointSystem;
+  export type PointSystem = DatabaseTypes.PointSystem;
+
+  export const Season = DatabaseTypes.Season;
+  export type Seaons = DatabaseTypes.Season;
 };
 
 export { DatabaseTypes, ObjectTypes };
@@ -30,51 +91,7 @@ const ErrorResponse = z.object({
 });
 type ErrorResponse = z.infer<typeof ErrorResponse>;
 
-const CircuitLayout = z.object({
-  id: z.string().uuid(),
-  name: z.string(),
-  first_year: z.number().int(),
-  last_year: z.number().int(),
-  track_length: z.number(),
-  race_lap_record: z.object({
-    time: z.string().regex(new RegExp("^d{1}:d{2}.d{3}$")),
-    driver_id: z.string().uuid(),
-  }),
-});
-type CircuitLayout = z.infer<typeof CircuitLayout>;
-
-const Circuit = z.object({
-  id: z.string().uuid(),
-  name: z.string(),
-  country: z.string().length(2),
-  date_opened: z.string().date(),
-  layout: CircuitLayout.array().min(1),
-});
-type Circuit = z.infer<typeof Circuit>;
-
-const GPWeekend = z.object({
-  id: z.string().uuid(),
-  start_date: z.string().date(),
-  end_date: z.string().date(),
-  circuit: z.string().uuid(),
-  sessions: z.string().uuid().array().min(1),
-  driver_entries: z.string().uuid().array().min(1),
-  team_entries: z.string().uuid().array().min(1),
-});
-type GPWeekend = z.infer<typeof GPWeekend>;
-
-const PointSystem = z.object({
-  awards_extra_point_for_fastest_lap_t10: z.boolean(),
-});
-type PointSystem = z.infer<typeof PointSystem>;
-
-const Season = z.object({
-  drivers: z.string().uuid().array(),
-  num_of_rounds: z.number().int(),
-  is_current_season: z.boolean().default(false),
-  point_system: PointSystem,
-});
-type Season = z.infer<typeof Season>;
+export { ErrorResponse };
 
 function getPagedResponse<T extends z.ZodTypeAny>(schema: T) {
   return z.object({
@@ -85,5 +102,3 @@ function getPagedResponse<T extends z.ZodTypeAny>(schema: T) {
     items: schema.array()
   });
 }
-
-export { ErrorResponse, CircuitLayout, Circuit, GPWeekend, Season };
