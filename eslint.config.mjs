7be76a1a0f52b05b8bf47d79cb0a907d810522da
Daniel Luna <git@danielluna.nyc>
2024-12-28 16:21:39 -0500

    Prepare Eslint for use with astro

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+// import eslintPluginAstro from 'eslint-plugin-astro';
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
@@ -8,9 +9,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  {
-    ignores: ['src/client/env.d.ts'],
-  },
+  // ...eslintPluginAstro.configs.recommended,
   {
     languageOptions: {
       parserOptions: {
@@ -22,6 +21,13 @@ export default tseslint.config(
   {
     rules: {
       'sort-imports': 'error',
+    },
+  },
+  // Allow triple-slash references in `*.d.ts` files.
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 'off',
     },
   },
   prettierConfig
