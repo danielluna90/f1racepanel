008aa586cb355c022b954ef03b51a4c04b84c78e
Daniel Luna <git@danielluna.nyc>
2025-01-22 08:07:47 -0600

    Remove spread operator

--- src/client/services/utils.ts
+++ src/client/services/utils.ts
@@ -1,5 +1,7 @@
 export const getFlagEmoji = (countryCode: string): string => {
-  return [...countryCode.toUpperCase()]
+  return countryCode
+    .toUpperCase()
+    .split('')
     .map(char => String.fromCodePoint(127397 + char.charCodeAt(0)))
     .reduce((a, b) => `${a}${b}`);
 };
